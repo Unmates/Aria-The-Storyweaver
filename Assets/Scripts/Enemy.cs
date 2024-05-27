042b7b5bb3e17370981c8e792287a27b457f12b4
Unmates <farellishaktibaihaqi@gmail.com>
2024-05-27 14:10:12 +0700

    add enemy ai and rahwana sprite

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -25,14 +25,6 @@ public class Enemy : MonoBehaviour
     public void takedamage(float damage)
     {
         currenthp -= damage;
-        //if (animator != null)
-        //{
-        //    animator.SetTrigger("hurt");
-        //}
-        //else
-        //{
-        //    Debug.LogWarning("Animator not assigned. Hurt animation not played.");
-        //}
         animator.SetTrigger("Hurt");
 
         if (currenthp < 0)
@@ -43,14 +35,6 @@ public class Enemy : MonoBehaviour
 
     void dead()
     {
-        //if (animator != null)
-        //{
-        //    animator.SetBool("dead", true);
-        //}
-        //else
-        //{
-        //    Debug.LogWarning("Animator not assigned. Dead animation not played.");
-        //}
         animator.SetBool("Dead", true);
         Debug.Log("Enemy dead");
         GetComponent<Collider2D>().enabled = false;
