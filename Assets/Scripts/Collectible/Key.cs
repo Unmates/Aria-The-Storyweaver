d2540cf991110b4a7cf606d8bdb2d3eb9ef8d3ff
Unmates <farellishaktibaihaqi@gmail.com>
2024-06-17 23:47:14 +0700

    add door (done)

--- Assets/Scripts/Collectible/Key.cs
+++ Assets/Scripts/Collectible/Key.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyCollect : MonoBehaviour
+public class Key : MonoBehaviour
 {
-    public class Key : MonoBehaviour
-    {
-        public Door door; // Reference to the door to be opened
+    [SerializeField] GameObject doorObj;
+    Door door;
+    [SerializeField] AudioClip collectSound;
 
-        private void OnTriggerEnter2D(Collider2D other)
+    private void Start()
+    {
+        door = doorObj.GetComponent<Door>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
         {
-            if (other.CompareTag("Player"))
-            {
-                door.OpenDoor();
-                Destroy(gameObject); // Destroy the key after it's picked up
-            }
+            SoundsManager.instance.PlaySound(collectSound);
+            door.OpenDoor();
+            Destroy(gameObject);
         }
     }
 }
